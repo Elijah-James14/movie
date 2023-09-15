08513941cd9e8fbc4705d5c1c2bfafeff2f7f069
Elijah James <elijah.jamesmay14@gmail.com>
2023-09-16 00:38:25 +0300

    Edited the homepage

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -1,7 +1,40 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, Button } from "@chakra-ui/react";
+import useMovies from "../hooks/useMovies";
+import { Link } from "react-router-dom";
+import { AiFillPlayCircle } from "react-icons/ai";
 
 const FeaturedMovie = () => {
-  return <></>;
+  const { data, isError } = useMovies();
+  if (!data) return null;
+  const value = data.results[Math.floor(Math.random() * data.results.length)];
+  if (isError) return <Text>There is something wrong.</Text>;
+
+  return (
+    <>
+      <Box marginBottom="20px" padding="20px" position={"relative"}>
+        <Image
+          src={`https://image.tmdb.org/t/p/w500/${value.poster_path}`}
+          width="100vw"
+          height="500px"
+          objectFit="fill"
+        />
+        <Box position="absolute" top="50%" left="70px" textAlign={"left"}>
+          <Link to={"/movies/" + value.id}>
+            {" "}
+            <Button
+              variant={"solid"}
+              backgroundColor="red.600"
+              _hover={{ backgroundColor: "red" }}
+              textColor="white"
+              leftIcon={<AiFillPlayCircle />}
+            >
+              Watch Trailer
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default FeaturedMovie;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,54 +1,19 @@
-import { AiFillPlayCircle } from "react-icons/ai";
 import MovieGrid from "../components/MovieGrid";
-import {
-  HStack,
-  Heading,
-  Grid,
-  GridItem,
-  Box,
-  Image,
-  Text,
-  Button,
-} from "@chakra-ui/react";
+import { HStack, Heading, Grid, GridItem } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
-import useMovies from "../hooks/useMovies";
-import { Link } from "react-router-dom";
+import FeaturedMovie from "../components/FeaturedMovie";
 const HomePage = () => {
   document.body.style.backgroundColor = "white";
 
-  const { data, isError } = useMovies();
-  if (!data) return null;
-  const value = data.results[Math.floor(Math.random() * data.results.length)];
-  if (isError) return <Text>There is something wrong.</Text>;
-
   return (
     <>
-      <Box marginBottom="20px" padding="20px" position={"relative"}>
-        <Image
-          src={`https://image.tmdb.org/t/p/w500/${value.poster_path}`}
-          width="100vw"
-          height="300px"
-          objectFit="fill"
-        />
-        <Box position="absolute" top="50%" left="70px" textAlign={"left"}>
-          <Link to={"/movies/" + value.id}>
-            {" "}
-            <Button
-              variant={"solid"}
-              backgroundColor="red.600"
-              _hover={{ backgroundColor: "red" }}
-              textColor="white"
-              leftIcon={<AiFillPlayCircle />}
-            >
-              Watch Trailer
-            </Button>
-          </Link>
-        </Box>
-      </Box>
       <Grid
-        templateAreas={{ base: `"main"`, lg: `"featured main"` }}
-        templateRows={{ base: "1fr", lg: "200px 1fr" }}
+        templateAreas={`"featured" "main"`}
+        templateColumns={`"500px""1fr"`}
       >
+        <GridItem area="featured">
+          <FeaturedMovie />
+        </GridItem>
         <GridItem area="main">
           <HStack
             textColor={"black"}
